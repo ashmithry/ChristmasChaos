180214d8048d1ea3fbb40c1bee03c262b32a472a
ashmithry <ashuyaramada@gmail.com>
2024-12-29 13:07:55 -0500

    Player Movement Finished

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    private Rigidbody2D rb;
+    public Transform sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xmov = Input.GetAxisRaw("Horizontal");
+        float ymov = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(xmov, ymov) * moveSpeed;
+
+        if (xmov < 0)
+        {
+            sprite.localScale = new Vector3(-0.5f, 0.5f, 1);
+        }
+        else if (xmov > 0){
+            sprite.localScale = new Vector3(0.5f, 0.5f, 1);
+        }
+
+
+    }
+}
